d8eed6cc59e3637cec669fd620f0ed90a3f34638
FloatingComet62 <shubh2007rai@gmail.com>
2024-07-02 21:44:46 +0530

    log fix

--- lib/log.c
+++ lib/log.c
@@ -13,14 +13,14 @@ void log_debugwarn(const char* message, const char* file, const u32 line_number)
 	if (COMET_LIB_LOG_LEVEL > CLLL_WARN) {
 		return;
 	}
-	printf("[\033[1;33mInfo\033[0m][%s:%d] %s\n", file, line_number, message);
+	printf("[\033[1;33mWarn\033[0m][%s:%d] %s\n", file, line_number, message);
 }
 
 void log_debugerrr(const char* message, const char* file, const u32 line_number) {
 	if (COMET_LIB_LOG_LEVEL > CLLL_ERROR) {
 		return;
 	}
-	printf("[\033[1;31mInfo\033[0m][%s:%d] %s\n", file, line_number, message);
+	printf("[\033[1;31mError\033[0m][%s:%d] %s\n", file, line_number, message);
 }
 
 #else
@@ -36,14 +36,14 @@ void log_warn(const char* message) {
 	if (COMET_LIB_LOG_LEVEL > CLLL_WARN) {
 		return;
 	}
-	printf("[\033[1;33mInfo\033[0m] %s\n", message);
+	printf("[\033[1;33mWarn\033[0m] %s\n", message);
 }
 
 void log_errr(const char* message) {
 	if (COMET_LIB_LOG_LEVEL > CLLL_ERROR) {
 		return;
 	}
-	printf("[\033[1;31mInfo\033[0m] %s\n", message);
+	printf("[\033[1;31mError\033[0m] %s\n", message);
 }
 
 #endif
