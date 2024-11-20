22ca50c55c45e438d038ae83784ac2f1589846fe
FloatingComet62 <shubh2007rai@gmail.com>
2024-11-20 23:12:48 +0530

    concat fix

--- main.c
+++ main.c
@@ -5,16 +5,18 @@
 #else
 
 int main() {
-  // str s = str_init("Hello World");
-  // info(s.data);
-  // str_deinit(s);
+  str s = str_init("Hello ");
+  str s1 = str_init("World");
+  strMUT_concat(&s, s1);
+  info(s.data);
+  str_deinit(s);
 
-  graph g = graph_init(4);
-  int weight = 1.0;
-  graphMUT_add_bidirectional_edge(&g, 0, 1, optional_init_some(&weight));
-  graphMUT_add_edge(&g, 1, 2, optional_init_some(&weight));
-  printf("Degree of node 1: %d\n", graph_get_degree(g, 1));
-  graph_deinit(g);
+  // graph g = graph_init(4);
+  // int weight = 1.0;
+  // graphMUT_add_bidirectional_edge(&g, 0, 1, optional_init_some(&weight));
+  // graphMUT_add_edge(&g, 1, 2, optional_init_some(&weight));
+  // printf("Degree of node 1: %d\n", graph_get_degree(g, 1));
+  // graph_deinit(g);
 
   return 0;
 }
